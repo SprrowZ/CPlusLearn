1e5a3895a2016d3b3f5cdfb2030e87095755afd6
zzg <zhaozhenguo.zzg@alibaba-inc.com>
2022-03-07 20:59:13 +0800

    文件操作

--- cdemo/ctest.c
+++ cdemo/ctest.c
@@ -6,7 +6,9 @@
 //带参数的宏定义,括号很重要！
 #define M(y) (y*y+3*y)
 
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void demoPointer1() {
     int a = 1;
@@ -181,19 +183,202 @@ void judgePointer(int *p) {
 
     }
 }
-void demoPointer14(){
-    int a = 22,b= 33,c =99;
-    int *arr[3] = {&a,&b,&c};
+
+void demoPointer14() {
+    int a = 22, b = 33, c = 99;
+    int *arr[3] = {&a, &b, &c};
     //指向指针数组的指针
     int **p = arr;
-    printf("%d,%d,%d\n",*arr[0],*arr[1],*arr[2]);
-    printf("%d,%d,%d\n",**(p+0),**(p+1),**(p+2));
+    printf("%d,%d,%d\n", *arr[0], *arr[1], *arr[2]);
+    printf("%d,%d,%d\n", **(p + 0), **(p + 1), **(p + 2));
+}
+
+//测试函数指针
+int maxValue(int a, int b) {
+    return a > b ? a : b;
+}
+
+int demoPointer15() {
+    int x = 10, y = 12, maxVal;
+    int (*pmax)(int, int) = maxValue;
+    maxVal = maxValue(x, y);
+    printf("maxValue is:%d", maxVal);
+}
+
+/******************结构体*******************/
+struct Stu {
+    char *name;
+    int age;
+    int num;
+    float score;
+};
+//结构体数组
+struct Company {
+    char *name;
+    float gmv;
+    char *location;
+} company[8];
+
+
+void testStruct1() {
+    struct Stu stu1, stu2;
+    stu1.age = 12;
+    stu1.num = 1011;
+    stu1.score = 85.5;
+    printf("age:%d,num:%d,score:%f", stu1.age, stu1.num, stu1.score);
+}
+
+void testStruct2() {
+    struct Stu stu1;
+    stu1.age = 12;
+    stu1.num = 1011;
+    stu1.score = 85.5;
+
+    struct Stu *pstu1 = &stu1;
+    printf("age:%d,num:%d,score:%f", (*pstu1).age, pstu1->num, stu1.score);
+}
+
+struct Fruit {
+    char *name;
+    char *color;
+    int num;
+} fruits[] = {
+        {"Apple",      "red",    1},
+        {"Orange",     "yellow", 2},
+        {"Watermelon", "green",  3}
+};
+
+void average(struct Fruit *ps, int len);
+
+void testStruct3() {
+    int len = sizeof(fruits) / sizeof(struct Fruit);
+    average(fruits, len);
+}
+
+void average(struct Fruit *ps, int len) {
+    for (int i = 0; i < len; ++i) {
+        printf("name:%s,color:%s,num:%d\n", (ps + i)->name, (ps + i)->color, (ps + i)->num);
+    }
+}
+
+void demoEnum() {
+    enum week {
+        MON, Tues, Wed, Thurs, Fri, Sat, Sun
+    }; //枚举默认值从0开始，往后逐个加1
+    enum week2 {
+        MON2 = 1, Tues2, Wed2
+    };
+
+}
+
+//const
+
+void testConst1() {
+    const int MaxNum = 10;
+
+    int num1 = 10;
+    int num2 = 20;
+    const int *p1 = &num1;
+    p1 = &num2;
+    num2 = 3;
+    printf("*p1 value is:%d", *p1);
+    int const *p2 = &num2;
+    int *const p3 = &num1;
+    //p3 = &num2; //报错
+}
+
+void testConst2() {
+    const char *p1 = "hello!";
+    char *p2 = p1;//将const转化为非const，有问题
+    printf("p2 value:%s", *p2);
+}
+
+void testRand() {
+    int a, b;
+    srand((unsigned) time(NULL)); //重新播种
+    a = rand() % 10;  //产生0~9的随机数
+    b = rand() % 51 + 13; //产生13~63的随机数
+    printf("随机数：a:%d,b:%d", a, b);
+}
+
+void testFile1() {
+    FILE *fp = fopen("/Users/zhaozhenguo/Desktop/cdemo.txt", "r");
+    int N = 100;
+    char str[N + 1];
+    if (fp == NULL) {
+        printf("Failed to open file!\n");
+        exit(0);
+    } else {
+        printf("Open Success~\n");
+        while (fgets(str, N, fp) != NULL) {
+            printf("%s", str);
+        }
+
+    }
+    fclose(fp);
+}
+
+//文件读取fgetc
+void testFile2() {
+    char ch;
+    FILE *fp = fopen("/Users/zhaozhenguo/Desktop/cdemo.txt", "r+");
+    if (fp == NULL) {
+        printf("Fail to open file");
+        exit(0);
+    }
+    while ((ch = fgetc(fp)) != EOF) {
+        putchar(ch);
+    }
+    putchar("\n");
+    fclose(fp);
+}
+#define  N  101
+void testFile3() {
+    FILE *fp;
+    fp = fopen("/Users/zhaozhenguo/Desktop/cdemo.txt", "rt");
+    if (fp == NULL) {
+        printf("Fail to open file");
+        exit(0);
+    }
+//    char *append_content = "i want to fly~";
+//    fputs(append_content, fp);
+    char content[N+1];
+    while (fgets(content,N,fp)!=NULL){
+        printf("%s",content);
+    }
+    fclose(fp);
+
+}
+#define M 3
+struct FileStu{
+    char name[10];//name
+    int num; // number
+    int age;//age
+    float score;
+} boys[M] ={
+        "李一",1,20,78.5,
+        "王二",2,22,88.5,
+        "张三",3,24,99.5
+        },boy,*pboys;
+
+void testFile4() {
+    FILE  *fp;
+    int i;
+    pboys = boys;
+    if ((fp = fopen("/Users/zhaozhenguo/Desktop/cdemo.txt","wb+"))==NULL){
+        printf("Cannot open file!bizzzzzz~");
+        exit(1);
+    }
+    fwrite(boys,sizeof(struct FileStu),3,fp);
+    fseek(fp,sizeof (struct FileStu),SEEK_SET);//移动位置指针
+    fread(&boy,sizeof (struct  FileStu),1,fp);//读取一条学生信息到boy中
+    printf("%s %d %d %f\n",boy.name,boy.num,boy.age,boy.score);
+    fclose(fp);
 }
 
 
 int main() {
-    // printf("value:%d", M(3)/2);
-    demoPointer14();
+    testFile4();
     return 0;
 }
 
