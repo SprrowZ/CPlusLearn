a5b540eb8839f76ccb804de7496b16c78d77daf6
rye <1804124963@qq.com>
2022-03-23 01:00:29 +0800

    chapter4

--- demo1/chapter4.cpp
+++ demo1/chapter4.cpp
@@ -9,64 +9,73 @@ using namespace std;
 
 class People {
 public:
-    void setname(char *name);
-
-    void setage(int age);
-
-    char *getname();
-
-    int getage();
-
-private :
+    void show();
+protected:
     char *m_name;
     int m_age;
+
 };
-
-void People::setname(char *name) {
-    m_name = name;
+void  People::show() {
+ cout<<m_name<<"的年龄是："<<m_age<<endl;
 }
-
-void People::setage(int age) {
-    m_age = age;
-}
-
-char *People::getname() {
-    return m_name;
-}
-
-int People::getage() {
-    return m_age;
-}
-
 
 class Student : public People {
 public:
-    void setscore(float score);
-
-    float getscore();
-
-private:
+   void learning();
+public:
+    using People::m_name;//将protected改为public
+    using People::m_age; //将protected改为public
     float m_score;
+private:
+    using People::show; //将public改为private
 };
 
-void Student::setscore(float score) {
-    m_score = score;
-}
-
-float Student::getscore() {
-    return m_score;
+void Student::learning() {
+    cout<<"我是："<<m_name<<",今年"<<m_age<<"岁，这次考了"<<m_score<<endl;
 }
 
 void testX() {
     Student stu;
-    stu.setname("小明");
-    stu.setage(22);
-    stu.setscore(99.5);
-    cout << stu.getname() << "的年龄是：" << stu.getage() << "，成绩是：" << stu.getscore() << endl;
+    stu.m_name = "小明";
+    stu.m_age = 16;
+    stu.m_score = 99.5f;
+    //stu.show();//compile error
+    stu.learning();
 }
 
+
+class Human{
+protected:
+    char *m_name;
+    int m_age;
+public:
+    Human(char *,int);
+
+};
+Human::Human(char * name, int age):m_name(name),m_age(age) {}
+
+class Child:public Human{
+private:
+    float m_score;
+public:
+    Child(char*,int ,float);
+    void display();
+};
+
+Child::Child(char *name,int age,float score):Human(name,age),m_score(score){
+
+}
+void Child::display() {
+    cout<<m_name<<"的年龄是："<<m_age<<",其成绩为："<<m_score<<endl;
+}
+void testY(){
+    Child child("二狗",12,87.5);
+    child.display();
+}
+
+
 int main() {
-    testX();
+    testY();
     return 1;
 }
 
